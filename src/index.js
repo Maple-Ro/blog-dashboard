f2a7c8fbda6e11a2291ccb923a21625d56e358b6
liutsingluo <liutsingluo@163.com>
2017-04-13 14:45:20 +0800

    添加loading

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import dva from "dva";
 import "./index.css";
-import Users from "./models/users";
 import router from "./router";
+import createLoading from 'dva-loading';
+
 // 1. Initialize
 const app = dva();
 
 // 2. Plugins
-// app.use({});
+app.use(createLoading());
 
 // 3. Model
 // registerModel(app, Users);
